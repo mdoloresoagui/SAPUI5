f06e38d40aec0be43210a66bd611c1794a5b4bba
mdoloresoagui <mdoloresoagui@gmail.com>
2025-02-20 20:00:11 +0000

    Commit 02

--- webapp/controller/MainView.controller.js
+++ webapp/controller/MainView.controller.js
@@ -1,10 +1,29 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller"
-], (Controller) => {
-    "use strict";
+],
+    /**
+     * 
+     * @param {typeof sap.ui.core.mvc.Controller} Controller 
+     */
+    function (Controller) {
+        "use strict";
 
-    return Controller.extend("logaligroup.employees.controller.MainView", {
-        onInit() {
-        }
+        return Controller.extend("logaligroup.employees.controller.MainView", {
+            onInit() {
+            },
+            onLiveChangeEmpl: function () {
+                var EmplValue = this.byId("InputEmpl"),
+                    textEmpl = EmplValue.getValue();
+
+                if (textEmpl.length === 6) {
+                    //EmplValue.setDescription("OK");
+                    this.byId("LbCountry").setVisible(true);
+                    this.byId("slCountry").setVisible(true);
+                } else {
+                    //EmplValue.setDescription("Not OK");
+                    this.byId("LbCountry").setVisible(false);
+                    this.byId("slCountry").setVisible(false);
+                };
+            }
+        });
     });
-});
