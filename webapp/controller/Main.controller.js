63cc8aabaefb01a36f1cd3f053415fb7383446a8
mdoloresoagui <mdoloresoagui@gmail.com>
2025-03-01 10:58:09 +0000

    Commit_04_Layouts

---
+++ webapp/controller/Main.controller.js
@@ -0,0 +1,43 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
+],
+    function (Controller, JSONModel) {
+        return Controller.extend("logaligroup.employees.controller.Main", {
+            onInit: function () {
+                var oJSONModelEmp = new JSONModel(),
+                    oJSONModelCtr = new JSONModel(),
+                    oJSONModelLayout = new JSONModel(),
+                    oView = this.getView();
+
+                oJSONModelEmp.loadData("./localService/mockdata/Employees.json", false);
+                oView.setModel(oJSONModelEmp, "Employee");
+
+                oJSONModelCtr.loadData("./localService/mockdata/Countries.json", false);
+                oView.setModel(oJSONModelCtr, "Countries");
+
+                oJSONModelLayout.loadData("./localService/mockdata/Layouts.json", false);
+                oView.setModel(oJSONModelLayout, "Layouts");
+
+                //JSON - Configuración visibilidad Ciudad
+                var oJSONConfigView = new JSONModel({
+                    visibleID: true,
+                    visibleName: true,
+                    visibleCountry: true,
+                    visibleCity: false,
+                    visibleBtnCity: true,
+                    HideBtnCity: false
+                });
+                oView.setModel(oJSONConfigView, "ConfigView");
+ 
+                this._bus = sap.ui.getCore().getEventBus();
+                this._bus.subscribe("flexible", "showEmployee", this.showEmployeeDetail, this);
+            },
+            showEmployeeDetail: function(category,nameEvent,path){
+                var detailView = this.getView("").byId("detailEmpView");
+                detailView.bindElement("Employee>" + path);
+                //Modificar el Layout
+                this.getView().getModel("Layouts").setProperty("/ActiveKey", "TwoColumnsMidExpanded");
+            }
+        });
+    });
--- webapp/controller/MasterEmployee.controller.js
+++ webapp/controller/MasterEmployee.controller.js
@@ -1,40 +1,19 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/ui/model/json/JSONModel",
     "sap/ui/model/Filter",
     "sap/ui/model/FilterOperator"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller 
-     * @param {typeof sap.ui.model.json.JSONModel} JSONModel 
      * @param {typeof sap.ui.model.Filter} Filter 
      * @param {typeof sap.ui.model.FilterOperator} FilterOperator 
      */
-    function (Controller, JSONModel, Filter, FilterOperator) {
+    function (Controller, Filter, FilterOperator) {
         "use strict";
 
-        return Controller.extend("logaligroup.employees.controller.MainView", {
+        return Controller.extend("logaligroup.employees.controller.MasterEmployee", {
             onInit() {
-                var oJSONModelEmp = new JSONModel(),
-                    oJSONModelCtr = new JSONModel(),
-                    oView = this.getView();
-
-                oJSONModelEmp.loadData("./localService/mockdata/Employees.json", false);
-                oView.setModel(oJSONModelEmp, "Employee");
-
-                oJSONModelCtr.loadData("./localService/mockdata/Countries.json", false);
-                oView.setModel(oJSONModelCtr, "Countries");
-
-                //JSON - Configuración visibilidad Ciudad
-                var oJSONConfigView = new JSONModel({
-                    visibleID: true,
-                    visibleName: true,
-                    visibleCountry: true,
-                    visibleCity: false,
-                    visibleBtnCity: true,
-                    HideBtnCity: false
-                });
-                oView.setModel(oJSONConfigView, "ConfigView");
+                this._bus = sap.ui.getCore().getEventBus();
             },
             onFilter: function () {
                 var oJsonCtr = this.getView().getModel("Countries").getData(),
@@ -69,14 +48,13 @@ sap.ui.define([
                 oConfigView.setProperty("/visibleBtnCity", true);
                 oConfigView.setProperty("/HideBtnCity", false);
             },
-            
             showOrders: function (oEvent) {
                 //Obtenemos el item seleccionado
                 var oLinePress = oEvent.getSource(),
                     oContext = oLinePress.getBindingContext("Employee");
 
                 if (!this._oDialogOrder) {
-                    this._oDialogOrder = sap.ui.xmlfragment("logaligroup.employees.fragment.DialogOrders",this);
+                    this._oDialogOrder = sap.ui.xmlfragment("logaligroup.employees.fragment.DialogOrders", this);
                     this.getView().addDependent(this._oDialogOrder);
                 };
 
@@ -85,29 +63,10 @@ sap.ui.define([
             },
             onCloseOrders: function () {
                 this._oDialogOrder.close();
-            }
-
-            /*Deprecated
-            onLiveChangeEmpl: function () {
-                var EmplValue = this.byId("InputEmpl"),
-                    textEmpl = EmplValue.getValue();
-
-                if (textEmpl.length === 6) {
-                    //EmplValue.setDescription("OK");
-                    this.byId("LbCountry").setVisible(true);
-                    this.byId("slCountry").setVisible(true);
-                } else {
-                    //EmplValue.setDescription("Not OK");
-                    this.byId("LbCountry").setVisible(false);
-                    this.byId("slCountry").setVisible(false);
-                };
             },
-            showPostalCode: function (oEvent) {
-                var itemPressed = oEvent.getSource(),
-                    oContext = itemPressed.getBindingContext("Employee"),
-                    objectContext = oContext.getObject();
-
-                MessageToast.show(objectContext.PostalCode);
-            }*/
+            showEmpl: function (oEvent) {
+                var path= oEvent.getSource().getBindingContext("Employee").getPath();
+                this._bus.publish("flexible", "showEmployee", path);
+            }
         });
     });
